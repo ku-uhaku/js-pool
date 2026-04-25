3e772d63b1a6c730f8d697039ccf4bd133c8bbc9
kuuhaku <mohamedbarrahx@gmail.com>
2026-04-25 15:20:08 +0100

    EX24

---
+++ nasa.js
@@ -0,0 +1,17 @@
+const nasa = (N) => {
+  let res = []
+
+  for (let i = 1; i <= N; i++) {
+    if (i % 15 === 0) {
+      res.push("NASA")
+    } else if (i % 3 === 0) {
+      res.push("NA")
+    } else if (i % 5 === 0) {
+      res.push("SA")
+    } else {
+      res.push(String(i))
+    }
+  }
+
+  return res.join(" ")
+}
