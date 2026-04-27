c3600bfc5bc772cbda5299a3744b81a220225ae6
mbarrah <kuuhaku@kuu.com>
2026-04-27 10:13:08 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,22 @@
+const escapeStr = "\`\\\/\"\'";
+const arr = [4, "2"];
+const obj = {
+  str: "kuuhaku",
+  num: 10,
+  bool: false,
+  undef: undefined,
+};
+const nested = {
+  arr: [4, undefined, "2"],
+  obj: {
+    str: "kuuhaku",
+    num: 10,
+    bool: false,
+  },
+};
+
+Object.freeze(arr);
+Object.freeze(obj);
+Object.freeze(nested);
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
