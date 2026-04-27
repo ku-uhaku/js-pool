ae65354e017fa1ee42ec4e631380f90a91f042c4
mbarrah <kuuhaku@kuu.com>
2026-04-27 14:39:08 +0100

    physics

---
+++ physics.js
@@ -0,0 +1,11 @@
+const getAcceleration = (input) => {
+  if (input.f !== undefined && input.m !== undefined) {
+    return input.f / input.m;
+  } else if (input.Δv !== undefined && input.Δt !== undefined) {
+    return input.Δv / input.Δt;
+  } else if (input.d !== undefined && input.t !== undefined) {
+    return (2 * input.d) / input.t ** 2;
+  }
+
+  return "impossible";
+};
