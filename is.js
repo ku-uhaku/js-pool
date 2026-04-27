a7bb692656972fc328f4487a5edd79f597a2cf38
mbarrah <kuuhaku@kuu.com>
2026-04-27 14:17:26 +0100

    is

--- is.js
+++ is.js
@@ -3,7 +3,7 @@ const is = {};
 is.num = (input) => typeof input === "number";
 is.nan = (input) => Number.isNaN(input);
 is.str = (input) => typeof input === "string";
-is.bool = (input) => typeof input === "bool";
+is.bool = (input) => typeof input === "boolean";
 is.undef = (input) => typeof input === "undefined";
 is.def = (input) => typeof input !== "undefined";
 is.arr = (input) => Array.isArray(input);
