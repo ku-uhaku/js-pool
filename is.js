0889b3e7df181f38273780e49fb7d0d7bf470084
mbarrah <kuuhaku@kuu.com>
2026-04-27 14:21:21 +0100

    is

--- is.js
+++ is.js
@@ -7,9 +7,9 @@ is.bool = (input) => typeof input === "boolean";
 is.undef = (input) => typeof input === "undefined";
 is.def = (input) => typeof input !== "undefined";
 is.arr = (input) => Array.isArray(input);
-is.obj = (input) => typeof input === "object" && !Array.isArray(input) && typeof input !== null;
+is.obj = (input) => typeof input === "object" && !Array.isArray(input) &&  input !== null;
 is.fun = (input) => typeof input !== "function";
 is.truthy = (input) => !!input
 is.falsy = (input) => !input
 
-console.log(is.num(0))
+console.log(is.num({}))
