69a0636c9b61170b3f346d8c80c56b4e22da9859
mbarrah <kuuhaku@kuu.com>
2026-04-27 11:04:37 +0100

    last-first-kiss

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,5 +1,4 @@
 const first = (input) => input[0];
 const last = (input) => input[input.length - 1];
 
-const kiss = (input) => first(input) + last(input);
-
+const kiss = (input) => [last(input), first(input)];
