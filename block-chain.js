2a90ca1543461c5afa77204d102da7faeecdc39c
mbarrah <kuuhaku@kuu.com>
2026-04-28 10:11:43 +0100

    block-chain

--- block-chain.js
+++ block-chain.js
@@ -1,12 +1,5 @@
 const initchain = { index: 0, hash: "0" };
 
-const hashCode = (str) =>
-    (
-        [...str].reduce(
-            (h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h,
-            0,
-        ) >>> 0
-    ).toString(36);
 
 const blockChain = (data, prev = { index: 0, hash: "0" }) => {
     const index = prev.index + 1;
