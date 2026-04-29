7b1bdc9ed2c4c305a29b1ebd1658decb3f41047c
mbarrah <kuuhaku@kuu.com>
2026-04-29 17:59:10 +0100

    triangle

--- triangle.js
+++ triangle.js
@@ -1,8 +1,7 @@
-const triangle = (nb) => {
+const triangle = (str, nb) => {
     let res = [];
     for (let i = 1; i <= nb; i++) {
-        res.push("*".repeat(i));
+        res.push(str.repeat(i));
     }
     return res.join("\n");
 };
-
