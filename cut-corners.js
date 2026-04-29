b2860daabab7eb65e955ea873cbab414c36114f4
mbarrah <kuuhaku@kuu.com>
2026-04-29 14:36:49 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -8,9 +8,9 @@ const trunc = (n) => {
     while (i * 2 <= abs) i *= 2;
 
     // Phase 2: small i++ to land exactly (at most 1 step)
-    while (i <= abs) i++;
+    while (i+ 1 <= abs) i++;
 
-    return sign * (i - 1);
+    return sign * i ;
 };
 
 const floor = (nb) => (nb < 0 && trunc(nb) !== nb ? trunc(nb) - 1 : trunc(nb));
@@ -18,3 +18,37 @@ const floor = (nb) => (nb < 0 && trunc(nb) !== nb ? trunc(nb) - 1 : trunc(nb));
 const ceil = (nb) => (nb > 0 && trunc(nb) !== nb ? trunc(nb) + 1 : trunc(nb));
 
 const round = (nb) => floor(nb + 0.5);
+
+console.log("ROUND");
+console.log(round(4.4), Math.round(4.4));
+console.log(round(4.5), Math.round(4.5));
+console.log(round(4.6), Math.round(4.6));
+console.log(round(-1.4), Math.round(-1.4));
+console.log(round(-1.5), Math.round(-1.5));
+console.log(round(-1.6), Math.round(-1.6));
+console.log(round(-2.5), Math.round(-2.5));
+console.log(round(-2.6), Math.round(-2.6));
+
+console.log("CEIL");
+console.log(ceil(4.1), Math.ceil(4.1));
+console.log(ceil(4.9), Math.ceil(4.9));
+console.log(ceil(5), Math.ceil(5));
+console.log(ceil(-1.1), Math.ceil(-1.1));
+console.log(ceil(-1.9), Math.ceil(-1.9));
+console.log(ceil(-5), Math.ceil(-5));
+
+console.log("FLOOR");
+console.log(floor(4.1), Math.floor(4.1));
+console.log(floor(4.9), Math.floor(4.9));
+console.log(floor(5), Math.floor(5));
+console.log(floor(-1.1), Math.floor(-1.1));
+console.log(floor(-1.9), Math.floor(-1.9));
+console.log(floor(-5), Math.floor(-5));
+
+console.log("TRUNC");
+console.log(trunc(4.1), Math.trunc(4.1));
+console.log(trunc(4.9), Math.trunc(4.9));
+console.log(trunc(5), Math.trunc(5));
+console.log(trunc(-1.1), Math.trunc(-1.1));
+console.log(trunc(-1.9), Math.trunc(-1.9));
+console.log(trunc(-5), Math.trunc(-5));
