959db1ed6d944b6bf3b54f5072c8c58a377af727
mbarrah <kuuhaku@kuu.com>
2026-04-29 09:51:17 +0100

    repeat

--- collections.js
+++ collections.js
@@ -26,13 +26,12 @@ const arrToObj = (arr) => Object.assign({}, arr);
 const strToObj = (str) => Object.assign({}, str.split(""));
 
 const superTypeOf = (input) => {
+    if (input === null) return "null";
     if (input === undefined) return "undefined";
-    if (input == null) return "null";
-    console.log(typeof input);
     const fullType = Object.prototype.toString.call(input);
 
     let type = fullType.slice(8, -1).toLowerCase();
     return type[0].toUpperCase() + type.slice(1);
 };
-    
+
 console.log(superTypeOf(null));
