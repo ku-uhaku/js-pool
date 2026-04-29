5ba7231585043b0fc0a3a8a5575a6b072d236ee1
mbarrah <kuuhaku@kuu.com>
2026-04-29 13:29:17 +0100

    keep-cat

--- keep-cut.js
+++ keep-cut.js
@@ -8,4 +8,5 @@ const keepFirst = (str) => str.slice(0, 2);
 
 const keepLast = (str) => str.slice(-2);
 
-const keepFirstLast = (str) => str.length <= 2 ? str : keepFirst(str) + keepLast(str);
+const keepFirstLast = (str) =>
+    str.length <= 3 ? str : str.slice(0, 2) + str.slice(-2);
