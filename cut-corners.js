c2a3c4892381557abb325787bca46b6de4a260cf
mbarrah <kuuhaku@kuu.com>
2026-04-29 14:34:13 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,15 +1,14 @@
-const trunc = (nb) => parseInt(nb);
+const trunc = (n) => {
+    if (n === 0) return 0;
+    const sign = n < 0 ? -1 : 1;
+    const abs = sign * n;
+    let i = 0;
+    while (i <= abs) i++;
+    return sign * (i - 1);
+};
 
 const floor = (nb) => (nb < 0 && trunc(nb) !== nb ? trunc(nb) - 1 : trunc(nb));
 
 const ceil = (nb) => (nb > 0 && trunc(nb) !== nb ? trunc(nb) + 1 : trunc(nb));
 
-const round = (nb) =>
-    nb >= 0
-        ? nb - trunc(nb) >= 0.5
-            ? trunc(nb) + 1
-            : trunc(nb)
-        : trunc(nb) - nb > 0.5
-          ? trunc(nb) - 1
-          : trunc(nb);
-
+const round = (nb) => floor(nb + 0.5);
