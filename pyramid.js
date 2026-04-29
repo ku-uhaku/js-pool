0dfe9477d6e1d1b008370500754b4963138e418e
mbarrah <kuuhaku@kuu.com>
2026-04-29 18:13:07 +0100

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -2,10 +2,11 @@ const pyramid = (str, nb) => {
     let res = [];
 
     for (let i = 1; i <= nb; i++) {
-        let spaces = ((nb - i) / 2) * str.length;
-        res.push(" ".repeat(spaces) + str.repeat(i));
+        let count = i * 2 - 1;                 
+        let spaces = (nb - i) * str.length;   
+
+        res.push(" ".repeat(spaces) + str.repeat(count));
     }
 
     return res.join("\n");
 };
-
