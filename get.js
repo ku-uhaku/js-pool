ed745d3e615b0a42c2d47d7efccb5534c503870d
mbarrah <kuuhaku@kuu.com>
2026-04-29 17:36:48 +0100

    get

--- get.js
+++ get.js
@@ -2,8 +2,14 @@ const get = (src, path) => {
     let pathEle = path.split(".");
     let res = src;
     for (let ele of pathEle) {
+        if (res == null || !(ele in res)) {
+            return undefined;
+        }
         res = res[ele];
     }
     return res;
 };
 
+const src = { nested: { key: "peekaboo" } };
+const path = "nested.key"; // -> 'peekaboo'
+console.log(get(src, path));
